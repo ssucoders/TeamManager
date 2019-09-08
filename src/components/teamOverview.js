ef39ef75d134eb6e21d75632b1158299abe575f3
Muhammad Shahzad <muhammad.shahzad@teo-intl.com>
2019-09-08 21:28:57 +0500

    added users in team overview

--- src/components/teamOverview.js
+++ src/components/teamOverview.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import Team from "./team";
+import User from "./user";
 
 class TeamOverview extends Component{
     constructor(props){
@@ -15,6 +16,11 @@ class TeamOverview extends Component{
                     <React.Fragment>
                         <h2>{team.name} Overview</h2>
                         <Team item={team}/>
+                        <div className="card-group">
+                            {team.members.map((el, i)=>{
+                                return (<User item={el} key={i} />)
+                            })}
+                        </div>
                     </React.Fragment>
             :null}
             </React.Fragment>
