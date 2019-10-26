a2e483dfef9403706c0838bfc0c08f4301bbfab4
mUbaidAwan <uawan2385@gmail.com>
2019-10-26 20:33:47 +0500

    chage class into classname

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -16,11 +16,11 @@ class Welcome extends Component {
 
         return (
             <React.Fragment>
-                    <h4 class="media align-items-center font-weight-bold py-3 mb-4">
-                        <img src="/products/appwork/v131/assets_/img/avatars/1.png" alt="" class="ui-w-50 rounded-circle" />
-                        <div class="media-body ml-3">
+                    <h4 className="media align-items-center font-weight-bold py-3 mb-4">
+                        <img src="/products/appwork/v131/assets_/img/avatars/1.png" alt="" className="ui-w-50 rounded-circle" />
+                        <div className="media-body ml-3">
                             Welcome back, Mike!
-                            <div class="text-muted text-tiny mt-1"><small class="font-weight-normal">Today is {getToday(true)}</small></div>
+                            <div className="text-muted text-tiny mt-1"><small className="font-weight-normal">Today is {getToday(true)}</small></div>
                         </div>
                     </h4>
             </React.Fragment>
