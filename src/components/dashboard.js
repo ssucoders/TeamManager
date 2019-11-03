f2c17a124a3bfdae588a98ec43fcaafa9f83d129
mUbaidAwan <uawan2385@gmail.com>
2019-11-03 19:41:52 +0500

    create proper deck for menu

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -13,7 +13,7 @@ class DashBoard extends Component {
             <React.Fragment>
                 <div className="row">
                     <div className="col-lg-3 leftPanel">
-                        <div className="row">
+                        <div className="row ml-4">
                             <a href="#" id="logo" title="My Team">
                                 <img src="images/logo.png" />
                                 <div className="logoText">
@@ -22,133 +22,68 @@ class DashBoard extends Component {
                                 </div>
                             </a>
                         </div>
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Active</a>
+                        <ul className="nav flex-column ml-5 mt-4">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Overviews</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">My Teams</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Tasks</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Report</a>
                             </li>
-                        </ul> 
+                            <li className="nav-item">
+                                <a className="nav-link " href="#" >Share</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link " href="#" >Mail</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link " href="#" >Settings</a>
+                            </li>
+                        </ul>
                     </div>
                     <div className="col-lg-9 rightPanel">
-                        <h1>My Teams</h1>
-                            <div className="card-deck">
-                                    <div className="card">
-                                        <div className="row no-gutters">
-                                            <div className="col-md-8">
-                                                <div className="card-body">
-                                                    <h4 className="card-title">28</h4>
-                                                    <p className="card-text"><h5>Total Team Members</h5><small className="text-muted">Excellent Designer</small></p>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-4  mt-5">
-                                                <img src="/images/logo.png" className="card-img-top" alt="..." />
-                                            </div>
+                        <h1 className="ml-3">My Teams</h1>
+                        <div className="card-deck upperDecks">
+                            <div className="card">
+                                <div className="row no-gutters">
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h4 className="card-title">28</h4>
+                                            <h6 className="card-text topText">Total Team Members</h6>
+                                            <p className="text-muted crdText mb-0"><small>Excellent Designer</small></p>
                                         </div>
                                     </div>
-                                    <div className="card">
-                                        <div className="row no-gutters">
-                                            <div className="col-md-8">
-                                                <div className="card-body">
-                                                    <h4 className="card-title">12k<small className="text-muted  h6">+26%</small></h4>
-                                                    <small className="card-text">Followers Of The Team</small>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-4 mt-5">
-                                                <img src="/images/logo.png" className="card-img-top" alt="..." />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="card">
-                                        <div className="row no-gutters">
-                                            <div className="col-md-8">
-                                                <div className="card-body">
-                                                    <h4 className="card-title">12</h4>
-                                                    <ul className="card-text"><b>Tasks</b>
-                                                        <li><small>UI/UX</small></li>
-                                                        <li><small>Painting</small></li>
-                                                        <li><small>Brand Design</small></li>
-                                                        <li><small>Animation</small></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-4 mt-5">
-                                                <img src="/images/logo.png" className="card-img-top" alt="..." />
-                                            </div>
-                                        </div>
+                                    <div className="col-md-4 mt-5">
+                                        <img src="/images/logo.png" className="card-img-top" alt="..." />
                                     </div>
                                 </div>
-                            <h2>Players</h2>
-                            <small>October 29,2019 Tuesday</small>
-                            <div className="card-deck">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <div className="row no-gutters">
-                                            <div className="col-md-2">
-                                                <img src="/images/logo.png" className="card-img-top" alt="..." />
-                                            </div>
-                                            <div className="col-md-9">
-                                               <h5>ubaid awan</h5>
-                                               <p>abc@gmail.com</p>
-                                            </div>
-                                            <div className="col-md-1">
-<div class="btn-group">
-  <a href="#" class="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    ...
-  </a>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="#">Separated link</a>
-  </div>
-</div>
-                                            </div>
-                                        </div>
-                                        <div className="clear"></div>
-                                        <div className="row">
-                                            <div className="col-md-4">
-                                                try
-                                            </div>
-                                            <div className="col-md-4">
-                                                try
-                                            </div>
-                                            <div className="col-md-4">
-                                                try
-                                            </div>
+                            </div>
+                            <div className="card">
+                                <div className="row no-gutters">
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h4 className="card-title">12k<small className="text-muted  h6">+26%</small></h4>
+                                            <h6 className="card-text topText">Followers Of The Team</h6>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="card">
-                                    <div className="row no-gutters">
-                                        <div className="col-md-4">
-                                            <img src="/images/logo.png" className="card-img-top" alt="..." />
-                                        </div>
-                                        <div className="col-md-8">
-                                            <div className="card-body">
-                                                <h4 className="card-title">12k<small className="text-muted  h6">+26%</small></h4>
-                                                <small className="card-text">Followers Of The Team</small>
-                                            </div>
-                                        </div>
+                                    <div className="col-md-4 mt-5">
+                                        <img src="/images/logo.png" className="card-img-top" alt="..." />
                                     </div>
                                 </div>
-                                <div className="card">
-                                    <div className="row no-gutters">
-                                        <div className="col-md-4">
-                                            <img src="/images/logo.png" className="card-img-top" alt="..." />
-                                        </div>
-                                        <div className="col-md-8">
-                                            <div className="card-body">
-                                                <h4 className="card-title">12</h4>
-                                                <ul className="card-text"><b>Tasks</b>
+                            </div>
+                            <div className="card">
+                                <div className="row no-gutters">
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h4 className="card-title">12</h4>
+                                            <div className="card-text">
+                                                <h6 className="font-weight-bold">Team Tasks</h6>
+                                                <ul className="pl-3">
                                                     <li><small>UI/UX</small></li>
                                                     <li><small>Painting</small></li>
                                                     <li><small>Brand Design</small></li>
@@ -157,8 +92,91 @@ class DashBoard extends Component {
                                             </div>
                                         </div>
                                     </div>
+                                    <div className="col-md-4 mt-5">
+                                        <img src="/images/logo.png" className="card-img-top" alt="..." />
+                                    </div>
                                 </div>
                             </div>
+                        </div>
+                        <h2>Players</h2>
+                        <small>October 29,2019 Tuesday</small>
+                         <div className="card-deck">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className="row no-gutters">
+                                        <div className="col-md-2">
+                                            <img src="/images/logo.png" className="card-img-top" alt="..." />
+                                        </div>
+                                        <div className="col-md-9">
+                                            <div className="ml-2">
+                                                <h6><small className="font-weight-bold">Rohan Rodriguez</small></h6>
+                                                <p className="text-muted m-0 lowerDeckText"><small>abc@gmail.com</small></p>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-1">
+                                            <div class="btn-group">
+                                                <a href="#" class="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    ...
+                                                 </a>
+                                                <div class="dropdown-menu">
+                                                    <a class="dropdown-item" href="#">Action</a>
+                                                    <a class="dropdown-item" href="#">Another action</a>
+                                                    <a class="dropdown-item" href="#">Something else here</a>
+                                                    <div class="dropdown-divider"></div>
+                                                    <a class="dropdown-item" href="#">Separated link</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="clear"></div>
+                                    <div className="row mt-2">
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Followers</small><br/>
+                                            <small className="font-weight-bold">12K</small>
+                                            </div>
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Works</small><br/>
+                                            <small className="font-weight-bold bg-light text-dark">12</small>
+                                            </div>
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Task</small><br/>
+                                            <small className="font-weight-bold">02</small>
+                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="row no-gutters">
+                                    <div className="col-md-4">
+                                        <img src="/images/logo.png" className="card-img-top" alt="..." />
+                                    </div>
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h4 className="card-title">12k<small className="text-muted  h6">+26%</small></h4>
+                                            <small className="card-text">Followers Of The Team</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="row no-gutters">
+                                    <div className="col-md-4">
+                                        <img src="/images/logo.png" className="card-img-top" alt="..." />
+                                    </div>
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h4 className="card-title">12</h4>
+                                            <ul className="card-text"><b>Tasks</b>
+                                                <li><small>UI/UX</small></li>
+                                                <li><small>Painting</small></li>
+                                                <li><small>Brand Design</small></li>
+                                                <li><small>Animation</small></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
