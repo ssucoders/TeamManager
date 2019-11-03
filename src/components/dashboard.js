3bd9b6644c6dd478add52f93ac70dc21d0f83ec5
mUbaidAwan <uawan2385@gmail.com>
2019-11-03 21:33:35 +0500

    make lower decks

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -47,7 +47,7 @@ class DashBoard extends Component {
                         </ul>
                     </div>
                     <div className="col-lg-9 rightPanel">
-                        <h1 className="ml-3">My Teams</h1>
+                        <h1 className="ml-3 font-weight-bold">My Teams</h1>
                         <div className="card-deck upperDecks">
                             <div className="card">
                                 <div className="row no-gutters">
@@ -98,9 +98,9 @@ class DashBoard extends Component {
                                 </div>
                             </div>
                         </div>
-                        <h2>Players</h2>
-                        <small>October 29,2019 Tuesday</small>
-                         <div className="card-deck">
+                        <h2 className="font-weight-bold">Players</h2>
+                        <p className="players text-muted"><small>October 29,2019 Tuesday</small></p>
+                        <div className="card-deck mt-3">
                             <div className="card">
                                 <div className="card-body">
                                     <div className="row no-gutters">
@@ -114,16 +114,16 @@ class DashBoard extends Component {
                                             </div>
                                         </div>
                                         <div className="col-md-1">
-                                            <div class="btn-group">
-                                                <a href="#" class="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <div className="btn-group">
+                                                <a href="#" className="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                     ...
                                                  </a>
-                                                <div class="dropdown-menu">
-                                                    <a class="dropdown-item" href="#">Action</a>
-                                                    <a class="dropdown-item" href="#">Another action</a>
-                                                    <a class="dropdown-item" href="#">Something else here</a>
-                                                    <div class="dropdown-divider"></div>
-                                                    <a class="dropdown-item" href="#">Separated link</a>
+                                                <div className="dropdown-menu">
+                                                    <a className="dropdown-item" href="#">Action</a>
+                                                    <a className="dropdown-item" href="#">Another action</a>
+                                                    <a className="dropdown-item" href="#">Something else here</a>
+                                                    <div className="dropdown-divider"></div>
+                                                    <a className="dropdown-item" href="#">Separated link</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -131,54 +131,171 @@ class DashBoard extends Component {
                                     <div className="clear"></div>
                                     <div className="row mt-2">
                                         <div className="col-md-4">
-                                            <small className="font-weight-bold lowerText">Followers</small><br/>
+                                            <small className="font-weight-bold lowerText">Followers</small><br />
                                             <small className="font-weight-bold">12K</small>
-                                            </div>
+                                            <p className="bg-light rounded-pill"><small className="font-weight-bold lowerText p-2">Top 1</small></p>
+                                        </div>
                                         <div className="col-md-4">
-                                            <small className="font-weight-bold lowerText">Works</small><br/>
-                                            <small className="font-weight-bold bg-light text-dark">12</small>
-                                            </div>
+                                            <small className="font-weight-bold lowerText">Works</small><br />
+                                            <small className="font-weight-bold">12</small>
+
+                                        </div>
                                         <div className="col-md-4">
-                                            <small className="font-weight-bold lowerText">Task</small><br/>
+                                            <small className="font-weight-bold lowerText">Task</small><br />
                                             <small className="font-weight-bold">02</small>
+                                        </div>
+                                    </div>
+                                    <div className="row mt-2">
+                                        <div className="col-md-4">
+                                            <p><small className="lowerText">UI/UX</small></p>
+                                            <p><small className="lowerText">Animation</small></p>
+                                            <p><small className="lowerText">Painting</small></p>
+                                        </div>
+                                        <div className="col-md-8">
+                                            <div className="progress mb-4 mt-1">
+                                                <div className="progress-bar bar1" role="progressbar" aria-valuenow="97" aria-valuemin="0" aria-valuemax="100">97%</div>
                                             </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <div className="row no-gutters">
-                                    <div className="col-md-4">
-                                        <img src="/images/logo.png" className="card-img-top" alt="..." />
-                                    </div>
-                                    <div className="col-md-8">
-                                        <div className="card-body">
-                                            <h4 className="card-title">12k<small className="text-muted  h6">+26%</small></h4>
-                                            <small className="card-text">Followers Of The Team</small>
+                                            <div className="progress mb-4">
+                                                <div className="progress-bar bar2" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">45%</div>
+                                            </div>
+                                            <div className="progress">
+                                                <div className="progress-bar bar3" role="progressbar" aria-valuenow="36" aria-valuemin="0" aria-valuemax="100">36%</div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div className="card">
-                                <div className="row no-gutters">
-                                    <div className="col-md-4">
-                                        <img src="/images/logo.png" className="card-img-top" alt="..." />
-                                    </div>
-                                    <div className="col-md-8">
-                                        <div className="card-body">
-                                            <h4 className="card-title">12</h4>
-                                            <ul className="card-text"><b>Tasks</b>
-                                                <li><small>UI/UX</small></li>
-                                                <li><small>Painting</small></li>
-                                                <li><small>Brand Design</small></li>
-                                                <li><small>Animation</small></li>
-                                            </ul>
+                                <div className="card-body">
+                                    <div className="row no-gutters">
+                                        <div className="col-md-2">
+                                            <img src="/images/logo.png" className="card-img-top" alt="..." />
+                                        </div>
+                                        <div className="col-md-9">
+                                            <div className="ml-2">
+                                                <h6><small className="font-weight-bold">Robert Gonzalez</small></h6>
+                                                <p className="text-muted m-0 lowerDeckText"><small>abc@gmail.com</small></p>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-1">
+                                            <div className="btn-group">
+                                                <a href="#" className="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    ...
+                                                 </a>
+                                                <div className="dropdown-menu">
+                                                    <a className="dropdown-item" href="#">Action</a>
+                                                    <a className="dropdown-item" href="#">Another action</a>
+                                                    <a className="dropdown-item" href="#">Something else here</a>
+                                                    <div className="dropdown-divider"></div>
+                                                    <a className="dropdown-item" href="#">Separated link</a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
+                                    <div className="clear"></div>
+                                    <div className="row mt-2">
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Followers</small><br />
+                                            <small className="font-weight-bold">11K</small>
+                                            <p className="bg-light rounded-pill"><small className="font-weight-bold lowerText p-2">Top 2</small></p>
+                                        </div>
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Works</small><br />
+                                            <small className="font-weight-bold">11</small>
+
+                                        </div>
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Task</small><br />
+                                            <small className="font-weight-bold">02</small>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-4">
+                                            <p><small className="lowerText">UI/UX</small></p>
+                                            <p><small className="lowerText">Animation</small></p>
+                                            <p><small className="lowerText">Painting</small></p>
+                                        </div>
+                                        <div className="col-md-8">
+                                            <div className="progress mb-4 mt-1">
+                                                <div className="progress-bar bar1" role="progressbar" aria-valuenow="97" aria-valuemin="0" aria-valuemax="100">97%</div>
+                                            </div>
+                                            <div className="progress mb-4">
+                                                <div className="progress-bar bar2" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">45%</div>
+                                            </div>
+                                            <div className="progress">
+                                                <div className="progress-bar bar3" role="progressbar" aria-valuenow="36" aria-valuemin="0" aria-valuemax="100">36%</div>
+                                            </div>
+                                        </div>
+                                   </div>
+                                </div>
+                            </div>
+                        <div className="card">
+                                <div className="card-body">
+                                    <div className="row no-gutters">
+                                        <div className="col-md-2">
+                                            <img src="/images/logo.png" className="card-img-top" alt="..." />
+                                        </div>
+                                        <div className="col-md-9">
+                                            <div className="ml-2">
+                                                <h6><small className="font-weight-bold">Christopher Robinson</small></h6>
+                                                <p className="text-muted m-0 lowerDeckText"><small>abc@gmail.com</small></p>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-1">
+                                            <div className="btn-group">
+                                                <a href="#" className="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    ...
+                                                 </a>
+                                                <div className="dropdown-menu">
+                                                    <a className="dropdown-item" href="#">Action</a>
+                                                    <a className="dropdown-item" href="#">Another action</a>
+                                                    <a className="dropdown-item" href="#">Something else here</a>
+                                                    <div className="dropdown-divider"></div>
+                                                    <a className="dropdown-item" href="#">Separated link</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="clear"></div>
+                                    <div className="row mt-2">
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Followers</small><br />
+                                            <small className="font-weight-bold">10K</small>
+                                            <p className="bg-light rounded-pill"><small className="font-weight-bold lowerText p-2">Top 3</small></p>
+                                        </div>
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Works</small><br />
+                                            <small className="font-weight-bold">10</small>
+
+                                        </div>
+                                        <div className="col-md-4">
+                                            <small className="font-weight-bold lowerText">Task</small><br />
+                                            <small className="font-weight-bold">02</small>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-4">
+                                            <p><small className="lowerText">UI/UX</small></p>
+                                            <p><small className="lowerText">Animation</small></p>
+                                            <p><small className="lowerText">Painting</small></p>
+                                        </div>
+                                        <div className="col-md-8">
+                                            <div className="progress mb-4 mt-1">
+                                                <div className="progress-bar bar1" role="progressbar" aria-valuenow="97" aria-valuemin="0" aria-valuemax="100">97%</div>
+                                            </div>
+                                            <div className="progress mb-4">
+                                                <div className="progress-bar bar2" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">45%</div>
+                                            </div>
+                                            <div className="progress">
+                                                <div className="progress-bar bar3" role="progressbar" aria-valuenow="36" aria-valuemin="0" aria-valuemax="100">36%</div>
+                                            </div>
+                                        </div>
+                                   </div>
                                 </div>
                             </div>
                         </div>
+                        </div>
                     </div>
-                </div>
             </React.Fragment>
 
         )
